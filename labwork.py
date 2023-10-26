b03935610e19fd4080edaa0fe0c51e300f6758b7
Hugo Liu <98724522+hugoliu-code@users.noreply.github.com>
2023-10-25 20:00:30 -0400

    Hugo's Work

--- labwork.py
+++ labwork.py
@@ -1,14 +1,17 @@
+
+
 def main():
     run = True
+    password = ""
     while run:
 
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
         choice = input("Please enter an option: ")
         
         if choice == "1":
-            encode()
+            password = encode()
         elif choice == "2":
-            print()
+            decode(password)
         elif choice == "3":
             run = False
 
@@ -18,11 +21,20 @@ def encode():
     str1 = ""
     user_input = input("Please enter your password to encode: ")
     for i in user_input:
-        num = int(i) + 3
+        num = (int(i) + 3)%10
         str1 = str1 + str(num)
     print("Your password has been encoded and stored!")
     print()
     return str1
+
+
+def decode(password):
+    # hugo's code
+    encoded = password
+    decoded = ""
+    for n in password:
+        decoded += str((int(n)+7)%10)
+    print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
 
 
 if __name__ == "__main__":
